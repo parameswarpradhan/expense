01b1258c7cbaff5ee51cd4defae821cc3443ff04
Parameswar Pradhan <parameswarpradhan@Parameswars-MacBook-Air.local>
2026-01-09 19:08:17 +0530

    feat: add LedgerEntry mongoose model

---
+++ backend/models/LedgerEntry.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+const ledgerEntrySchema = new mongoose.Schema(
+{
+  groupId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Group",
+    required: true,
+    index: true
+  },
+  type: {
+    type: String,
+    enum: [
+      "EXPENSE_CREATED",
+      "EXPENSE_ADJUSTED",
+      "EXPENSE_REVERSED",
+      "SETTLEMENT_CREATED",
+      "SETTLEMENT_COMPLETED"
+    ],
+    required: true
+  },
+  actor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  data: {
+    payer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    },
+    participants: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+      }
+    ],
+    splits: {
+      type: Map,
+      of: Number
+    },
+    amount: {
+      type: Number
+    },
+    referenceId: {
+      type: mongoose.Schema.Types.ObjectId
+    }
+  },
+  status: {
+    type: String,
+    enum: ["ACTIVE", "DISPUTED", "REVERSED"],
+    default: "ACTIVE"
+  }
+},
+{
+  timestamps: true
+}
+);
+module.exports = mongoose.model("LedgerEntry", ledgerEntrySchema);
